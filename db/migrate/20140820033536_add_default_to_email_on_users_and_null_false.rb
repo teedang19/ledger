42b026fa6435f792b898236622d47a5d6de2b324
Tam T Dang <teedang19@gmail.com>
2014-08-19 22:37:51 -0500

    email on Users is null false and default empty string per devise

---
+++ db/migrate/20140820033536_add_default_to_email_on_users_and_null_false.rb
@@ -0,0 +1,5 @@
+class AddDefaultToEmailOnUsersAndNullFalse < ActiveRecord::Migration
+  def change
+    change_column :users, :email, :string, null: false, default: ""
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140820033108) do
+ActiveRecord::Schema.define(version: 20140820033536) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 20140820033108) do
   create_table "users", force: true do |t|
     t.string   "first_name"
     t.string   "last_name"
-    t.string   "email"
+    t.string   "email",                  default: "", null: false
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "encrypted_password",     default: "", null: false
