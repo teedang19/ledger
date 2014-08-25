c1f7a1a0c6b2c6c9e09baa840e66b6da912eb0d9
Tam T Dang <teedang19@gmail.com>
2014-08-24 22:35:34 -0500

    after_sign_in_path goes to accounts_path #index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,12 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  private
+
+  def after_sign_in_path_for(resource)
+    accounts_path
+  end
+
   protected
 
   def configure_permitted_parameters
