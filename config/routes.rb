338b5e98916e5b7817bce82c212acd1f75d928bd
Tam T Dang <teedang19@gmail.com>
2014-08-24 21:49:01 -0500

    adjust spacing, add resources :accounts in routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,13 @@ require 'sidekiq/web'
 
 Rails.application.routes.draw do
   
+  root 'static_pages#home'
+
   devise_for :users, path: ''
+
+  resources :accounts
+  
   mount Sidekiq::Web => '/sidekiq'
   
-  root 'static_pages#home'
 
 end
