b3b7ab46ef44f2b4331f1bdf136b77faa0c01eb6
Tam T Dang <teedang19@gmail.com>
2014-08-27 17:48:34 -0500

    accounts_controller #new, #create, and #account_params (strong params)

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,2 +1,17 @@
 class AccountsController < ApplicationController
+
+  def new
+    @account = Account.new
+  end
+
+  def create
+    @account = Account.new(account_params)
+    render @account.save ? @account : 'new'
+  end
+
+  private
+
+  def account_params
+    params.require(:account).permit(:name, :type, :context, :balance)
+  end
 end
