06be5bfa8cf0696104759e4f89a05aec088ea2be
Tam T Dang <teedang19@gmail.com>
2014-09-02 20:15:43 -0500

    remove asset_account and debt_account models

--- app/models/asset_account.rb
+++
@@ -1,4 +0,0 @@
-class AssetAccount < Account
-  belongs_to :user
-  validates :context, inclusion: { in: ["Liquid", "Investment", "Insurance", "Other"] }
-end
--- app/models/debt_account.rb
+++
@@ -1,4 +0,0 @@
-class DebtAccount < Account
-  belongs_to :user
-  validates :context, inclusion: { in: ["Credit Card", "Student Loan", "Other"] }
-end
