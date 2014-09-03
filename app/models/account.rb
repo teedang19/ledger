924c85ab77fe9eddf786e305410c536591f4a5fc
Tam T Dang <teedang19@gmail.com>
2014-09-02 20:14:36 -0500

    account model validates context by classification, since no longer using STI

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,3 +1,17 @@
 class Account < ActiveRecord::Base
-  validates :type, inclusion: { in: ["DebtAccount", "AssetAccount"] }
+  belongs_to :user
+  validates :account_classification, inclusion: { in: ["Debt Account", "Asset Account"] }
+  validates :context_by_classification
+
+  private
+
+    def context_by_classification
+      case account_classification
+      when "Asset Account"
+        ["Liquid", "Investment", "Insurance", "Other"].include?(context)
+      when "Debt Account"
+        ["Credit Card", "Student Loan", "Other"].include?(context)
+      end
+    end
+    
 end
