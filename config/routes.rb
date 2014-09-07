af1e88e9d354f1cb15e5a882de1415e0cdb28abb
Tam T Dang <teedang19@gmail.com>
2014-09-07 13:35:32 -0500

    spatial changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
-  
+
   root 'static_pages#home'
 
   devise_for :users, path: ''
 
   resources :accounts
   
-  mount Sidekiq::Web => '/sidekiq'
-  
-
+  mount Sidekiq::Web => '/sidekiq'  
 end
