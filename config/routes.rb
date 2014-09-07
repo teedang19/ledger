ae311c0573386ab831177ffa95af2ca6deb62783
Tam T Dang <teedang19@gmail.com>
2014-09-07 13:43:05 -0500

    mount admin dashboard at /admin

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ require 'sidekiq/web'
 
 Rails.application.routes.draw do
 
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  
   root 'static_pages#home'
 
   devise_for :users, path: ''
