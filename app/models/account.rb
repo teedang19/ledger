e4487cc25f385cd116879dedff92a37fec0a7e87
Tam T Dang <teedang19@gmail.com>
2014-09-07 15:51:39 -0500

    rails_admin basic setup for account

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,5 +1,6 @@
 class Account < ActiveRecord::Base
   belongs_to :user
+  validates_presence_of :user_id
   validates :account_classification, inclusion: { in: ["Debt Account", "Asset Account"] }
   validate :context_by_classification
 
@@ -13,5 +14,56 @@ class Account < ActiveRecord::Base
         ["Credit Card", "Student Loan", "Other"].include?(context)
       end
     end
-    
+
+  rails_admin do
+
+    list do
+      field :user
+      field :name
+      field :balance
+      field :account_classification do
+        label do
+          "Type"
+        end
+      end
+      field :context
+    end
+
+    show do
+      field :user
+      field :name
+      field :balance
+      field :account_classification do
+        label do
+          "Type"
+        end
+      end
+      field :context
+    end
+
+    edit do
+      field :user do
+        read_only true
+      end
+      field :name
+      field :balance do
+        read_only true
+      end
+      field :account_classification, :enum do
+        label do
+          "Type"
+        end
+        enum do
+          ["Asset Account", "Debt Account"]
+        end
+      end
+      field :context, :enum do
+        enum do
+          ["Liquid", "Investment", "Insurance", "Other", "Credit Card", "Student Loan"]
+        end
+      end
+    end
+
+  end
+
 end
