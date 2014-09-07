024b70ab956c0309c559219c9212b9b4df047970
Tam T Dang <teedang19@gmail.com>
2014-09-07 15:23:54 -0500

    rescue from cancan access denied -- specifies '/' instead of root_url

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
   rescue_from CanCan::AccessDenied do |exception|
-    redirect_to root_url, :alert => "You are not authorized to access this page."
+    redirect_to '/', :alert => "You are not authorized to access this page."
   end
 
   private
