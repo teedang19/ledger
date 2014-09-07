41f16506975cfee27b341d677876bdd562a872cc
Tam T Dang <teedang19@gmail.com>
2014-09-07 15:10:34 -0500

    application controller rescues from cancan unauthorized error by redirecting to root_url with alert message of 'You are not authorized'

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,10 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
 
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_url, :alert => "You are not authorized to access this page."
+  end
+
   private
 
     def after_sign_in_path_for(resource)
