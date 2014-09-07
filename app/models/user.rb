51740d2d683c53b4021d1dac3cbf0c152663982e
Tam T Dang <teedang19@gmail.com>
2014-09-07 14:36:34 -0500

    user doesn't need email presence validation; devise handles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  validates_presence_of :first_name, :last_name, :email
+  validates_presence_of :first_name, :last_name
   # Include default devise modules. Others available are:
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
