55b10bfe84920456a5e51c23d9b75e9c627c1f73
Tam T Dang <teedang19@gmail.com>
2014-09-07 15:24:26 -0500

    ability - admin can access rails_admin and dashboard; non-admin can manage accounts where user_id is user.id

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,10 +4,12 @@ class Ability
   def initialize(user)
     user ||= User.new # guest user (not logged in)
     if user.admin?
+      can :access, :rails_admin
+      can :dashboard
       can :manage, :all
     else
-      can :manage, :all, user_id: user.id
+      can :manage, Account, user_id: user.id
     end
   end
-  
+
 end
