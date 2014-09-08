614c8abca2650ffb0d3a6436822ee6474050da30
Tam T Dang <teedang19@gmail.com>
2014-09-07 19:38:44 -0500

    correct validations on Account model

--- app/models/account.rb
+++ app/models/account.rb
@@ -3,15 +3,33 @@ class Account < ActiveRecord::Base
   validates_presence_of :user_id, :account_classification, :context, :balance
   validates :account_classification, inclusion: { in: ["Debt Account", "Asset Account"] }
   validate :context_by_classification
+  validate :balance_reflects_asset_or_debt
 
   private
 
     def context_by_classification
       case account_classification
       when "Asset Account"
-        ["Liquid", "Investment", "Insurance", "Other"].include?(context)
+        unless ["Liquid", "Investment", "Insurance", "Other"].include?(context)
+          errors.add(:account_classification, "must be either Liquid, Investment, Insurance, or Other for an Asset Account.")
+        end
       when "Debt Account"
-        ["Credit Card", "Student Loan", "Other"].include?(context)
+        unless ["Credit Card", "Student Loan", "Other"].include?(context)
+          errors.add(:account_classification, "must be either Credit Card, Student Load, or Other for a Debt Account.")
+        end
+      end
+    end
+
+    def balance_reflects_asset_or_debt
+      case account_classification
+      when "Asset Account"
+        unless balance > 0
+          errors.add(:balance, "must be positive for an Asset Account.")
+        end
+      when "Debt Account"
+        unless balance <= 0
+          errors.add(:balance, "must be negative for a Debt Account.")
+        end
       end
     end
 
